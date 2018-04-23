0b3c5d361f57cb2e6d64ea3367cd631103f9c94c
Arturas Molcanovas <a.molcanovas@gmail.com>
2018-04-23 14:18:16 +0100

    include shadydom polyfill in bundle

--- watch-series-helper.meta.js
+++ watch-series-helper.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @downloadURL https://raw.githubusercontent.com/AlorelUserscripts/watch-series-helper/master/watch-series-helper.user.js
 // @updateURL https://raw.githubusercontent.com/AlorelUserscripts/watch-series-helper/master/watch-series-helper.meta.js
-// @version 0.1.2
+// @version 0.1.3
 // @name WatchSeries helper
 // @namespace alorel.watch-series-helper
 // @author https://github.com/Alorel
